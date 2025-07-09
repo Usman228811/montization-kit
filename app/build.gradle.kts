7bbd2f36a4efbb3bdab24ede301fd3085cedf201
Usman Khan <appsapiconsole@gmail.com>
2025-07-09 16:12:34 +0500

    design change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.nas.internet.speedtest.meter.speed.test.meter.app"
+        applicationId = "com.test.ads"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
