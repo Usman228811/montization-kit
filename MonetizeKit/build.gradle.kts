2b6167a45a595a24fd251d0baa0fe1602cc14ac8
Usman_Khan <appspaiconsole@gmail.com>
2025-09-25 12:32:09 +0500

    v2.0.1 banner types added

--- MonetizeKit/build.gradle.kts
+++ MonetizeKit/build.gradle.kts
@@ -42,7 +42,7 @@ android {
 }
 
 group = "com.github.Usman228811"
-version = "v2.0.0"
+version = "v2.0.1"
 
 
 afterEvaluate {
@@ -52,7 +52,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.Usman228811"
                 artifactId = "MonetizeKit"
-                version = "v2.0.0"
+                version = "v2.0.1"
             }
         }
     }
