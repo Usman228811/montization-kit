5554c78ab742f1fc8032484e892f297cf42b0280
Usman_Khan <appspaiconsole@gmail.com>
2025-10-29 14:33:00 +0500

    v3.0.6 consume any native ad

--- MonetizeKit/build.gradle.kts
+++ MonetizeKit/build.gradle.kts
@@ -64,7 +64,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
-
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation(platform("androidx.compose:compose-bom:2025.09.01"))
     implementation("androidx.compose.ui:ui")
