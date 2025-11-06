3a05721fd9c59466225103c94a9e81cd6829110d
Usman_Khan <appspaiconsole@gmail.com>
2025-11-06 17:21:25 +0500

    3.0.9-meta

--- MonetizeKit/build.gradle.kts
+++ MonetizeKit/build.gradle.kts
@@ -42,7 +42,7 @@ android {
 }
 
 group = "com.github.Usman228811"
-version = "3.0.8-meta"
+version = "3.0.9-meta"
 
 
 afterEvaluate {
@@ -52,7 +52,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.Usman228811"
                 artifactId = "MonetizeKit"
-                version = "3.0.8-meta"
+                version = "3.0.9-meta"
             }
         }
     }
