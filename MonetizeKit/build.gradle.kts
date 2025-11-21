3c6d2d0e45b38d938901263f390b2b6831500584
Usman_Khan <appspaiconsole@gmail.com>
2025-11-21 10:26:10 +0500

    3.1.7-adapter

--- MonetizeKit/build.gradle.kts
+++ MonetizeKit/build.gradle.kts
@@ -42,7 +42,7 @@ android {
 }
 
 group = "com.github.Usman228811"
-version = "3.1.6-adapter"
+version = "3.1.7-adapter"
 
 
 afterEvaluate {
@@ -52,7 +52,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.Usman228811"
                 artifactId = "MonetizeKit"
-                version = "3.1.6-adapter"
+                version = "3.1.7-adapter"
             }
         }
     }
