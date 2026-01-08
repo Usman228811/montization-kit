a6bd44338f3036ca0f69fc42e0410d1c940b57ca
Usman_Khan <appspaiconsole@gmail.com>
2026-01-08 10:33:23 +0500

    3.2.2-meta added offer price support

--- MonetizeKit/build.gradle.kts
+++ MonetizeKit/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 group = "com.github.Usman228811"
-version = "3.2.1-meta"
+version = "3.2.2-meta"
 
 
 afterEvaluate {
@@ -50,7 +50,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.Usman228811"
                 artifactId = "MonetizeKit"
-                version = "3.2.1-meta"
+                version = "3.2.2-meta"
             }
         }
     }
