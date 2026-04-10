002c9ca81b5e06706636dccc00f0c07b151b07ca
Usman_Khan <appspaiconsole@gmail.com>
2026-04-10 12:04:10 +0500

    3.3.1-ng one time purchase issue resolved

--- MonetizeKit/build.gradle.kts
+++ MonetizeKit/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 group = "com.github.Usman228811"
-version = "3.2.93-ng"
+version = "3.3.1-ng"
 
 
 afterEvaluate {
@@ -50,7 +50,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.Usman228811"
                 artifactId = "MonetizeKit"
-                version = "3.2.93-ng"
+                version = "3.3.1-ng"
             }
         }
     }
