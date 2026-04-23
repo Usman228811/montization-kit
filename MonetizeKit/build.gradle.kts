5bdf3eb925c0912e6ccab59f0fd1b05da7501478
Usman_Khan <appspaiconsole@gmail.com>
2026-04-23 09:24:09 +0500

    3.3.4-meta subscription, oneTime-purchase changed

--- MonetizeKit/build.gradle.kts
+++ MonetizeKit/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 group = "com.github.Usman228811"
-version = "3.3.3-meta"
+version = "3.3.4-meta"
 
 
 afterEvaluate {
@@ -50,7 +50,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.Usman228811"
                 artifactId = "MonetizeKit"
-                version = "3.3.3-meta"
+                version = "3.3.4-meta"
             }
         }
     }
