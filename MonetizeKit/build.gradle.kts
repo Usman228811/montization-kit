50d02997b6c5f98087aa1f83cdc3d221bfab9c96
Usman_Khan <appspaiconsole@gmail.com>
2026-04-23 17:47:11 +0500

    3.3.6-adapter subscription, oneTime-purchase changed

--- MonetizeKit/build.gradle.kts
+++ MonetizeKit/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 group = "com.github.Usman228811"
-version = "3.3.5-adapter"
+version = "3.3.6-adapter"
 
 
 afterEvaluate {
@@ -50,7 +50,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.Usman228811"
                 artifactId = "MonetizeKit"
-                version = "3.3.5-adapter"
+                version = "3.3.6-adapter"
             }
         }
     }
