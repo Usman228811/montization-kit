999b682cc84fc9b8d098ca287da9ec89567be124
Usman_Khan <appspaiconsole@gmail.com>
2026-05-11 12:56:43 +0500

    3.4.3 collapsable banner enable false issue resolved

--- MonetizeKit/build.gradle.kts
+++ MonetizeKit/build.gradle.kts
@@ -18,6 +18,10 @@ android {
         consumerProguardFiles("consumer-rules.pro")
     }
 
+    publishing {
+        singleVariant("release") {}
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -40,7 +44,7 @@ android {
 }
 
 group = "com.github.Usman228811"
-version = "3.4.2"
+version = "3.4.3"
 
 
 afterEvaluate {
@@ -50,7 +54,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.Usman228811"
                 artifactId = "MonetizeKit"
-                version = "3.4.2"
+                version = "3.4.3"
             }
         }
     }
