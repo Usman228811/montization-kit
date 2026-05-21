6bfc52e3fa7711ce96bbfd5333145e9d9a058bd8
Usman_Khan <appspaiconsole@gmail.com>
2026-05-21 12:31:45 +0500

    3.4.7-rc consent manger canRequestAds removed

--- MonetizeKit/build.gradle.kts
+++ MonetizeKit/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 }
 
 group = "com.github.Usman228811"
-version = "3.4.6-rc"
+version = "3.4.7-rc"
 
 
 afterEvaluate {
@@ -53,7 +53,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.Usman228811"
                 artifactId = "MonetizeKit"
-                version = "3.4.6-rc"
+                version = "3.4.7-rc"
             }
         }
     }
