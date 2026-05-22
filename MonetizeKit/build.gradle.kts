b2d3d5dfc6c07d3e28b42a4a968c2f08f9feaaa6
Usman_Khan <appspaiconsole@gmail.com>
2026-05-22 16:25:11 +0500

    1.0.2-ng

--- MonetizeKit/build.gradle.kts
+++ MonetizeKit/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 }
 
 group = "com.github.Usman228811"
-version = "1.0.1-ng"
+version = "1.0.2-ng"
 
 
 afterEvaluate {
@@ -53,7 +53,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.Usman228811"
                 artifactId = "MonetizeKit"
-                version = "1.0.1-ng"
+                version = "1.0.2-ng"
             }
         }
     }
@@ -74,7 +74,7 @@ dependencies {
 
 
 //    implementation("com.google.android.gms:play-services-ads:24.9.0")
-    implementation("com.google.android.libraries.ads.mobile.sdk:ads-mobile-sdk:1.0.1")
+    implementation("com.google.android.libraries.ads.mobile.sdk:ads-mobile-sdk:1.1.0")
     implementation("com.intuit.sdp:sdp-android:1.1.1")
     implementation("com.intuit.ssp:ssp-android:1.1.1")
 
