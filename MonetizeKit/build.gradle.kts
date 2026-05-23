4f2639125806b0da88979d8df5158ad79dfa939f
Usman_Khan <appspaiconsole@gmail.com>
2026-05-23 15:13:58 +0500

    1.0.3-ng-rc remote config cache issue solved

--- MonetizeKit/build.gradle.kts
+++ MonetizeKit/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 }
 
 group = "com.github.Usman228811"
-version = "1.0.2-ng-rc"
+version = "1.0.3-ng-rc"
 
 
 afterEvaluate {
@@ -53,7 +53,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.Usman228811"
                 artifactId = "MonetizeKit"
-                version = "1.0.2-ng-rc"
+                version = "1.0.3-ng-rc"
             }
         }
     }
